52593684a0ebce5a4a8941f8f8a2b12ee65a5905
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-07-07 00:42:46 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MdocSecurity18013",
+    platforms: [.macOS(.v10_15), .iOS(.v16), .watchOS(.v9)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MdocSecurity18013",
+            targets: ["MdocSecurity18013"]),
+    ],
+    dependencies: [
+        .package(path: "../MdocDataModel18013"), 
+        .package(url: "https://github.com/apple/swift-log.git", branch: "main"),],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MdocSecurity18013", dependencies: ["MdocDataModel18013", .product(name: "Logging", package: "swift-log")]),
+        .testTarget(
+            name: "MdocSecurity18013Tests",
+            dependencies: ["MdocSecurity18013"]),
+    ]
+)
---
+++ Sources/MdocSecurity18013/SessionEncryption/SessionData.swift
@@ -0,0 +1,33 @@
+import Foundation
+import MdocDataModel18013
+import SwiftCBOR
+import Logging
+
+public struct SessionData {
+	let data: [UInt8]?
+	let status: UInt64?
+	
+	enum CodingKeys: String, CodingKey {
+		case data
+		case status
+	}
+}
+
+extension SessionData: CBORDecodable {
+	public init?(cbor: CBOR) {
+		guard case let .map(values) = cbor else { logger.error("Session data must be a map"); return nil  }
+		if case let .unsignedInt(s) = values[CodingKeys.status] { status = s } else { logger.info("SessionData: Missing status"); status = nil  }
+		if case let .byteString(bs) = values[CodingKeys.data] { data = bs } else { logger.error("SessionData: Missing data"); data = nil  }
+	}
+}
+
+extension SessionData: CBOREncodable {
+	public func toCBOR(options: CBOROptions) -> CBOR {
+		var res = [CBOR:CBOR]()
+		if let st = status { res[CBOR.utf8String(CodingKeys.status.rawValue)] = CBOR.unsignedInt(st) }
+		if let d = data { res[CBOR.utf8String(CodingKeys.data.rawValue)] = CBOR.byteString(d) }
+		return .map(res)
+	}
+}
+
+
