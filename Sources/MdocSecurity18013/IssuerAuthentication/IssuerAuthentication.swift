b88c54e26675acae6337e4bdc7e261b4ddce3c63
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-09-01 23:07:01 +0300

    remove unused temp. function

--- Sources/MdocSecurity18013/IssuerAuthentication/IssuerAuthentication.swift
+++ Sources/MdocSecurity18013/IssuerAuthentication/IssuerAuthentication.swift
@@ -57,25 +57,6 @@ public struct IssuerAuthentication {
 		}
 		return result
 	}
-	
-	/// Temporary function, mso should come from the server. Used to compute an MSO if not provided in sample data
-	public static func makeDefaultMSO(for document: Document, deviceKey: CoseKey) -> MobileSecurityObject? {
-		let dak = MobileSecurityObject.defaultDigestAlgorithmKind
-		guard let issuerNamespaces = document.issuerSigned.issuerNameSpaces?.nameSpaces else { return nil }
-		var vd = [NameSpace: DigestIDs]()
-		for (ns,items) in issuerNamespaces {
-			var dids = [DigestID: [UInt8]]()
-			for item in items {
-				let issuerSignedItemBytes = item.encode(options: CBOROptions()).taggedEncoded.encode()
-				let itemDigest = Self.getHash(dak, bytes: issuerSignedItemBytes)
-				dids[item.digestID] = [UInt8](itemDigest)
-			}
-			vd[ns] = DigestIDs(digestIDs: dids)
-		}
-		let valueDigests = ValueDigests(valueDigests: vd)
-		let validityInfo = ValidityInfo(signed: isoDateFormatter.string(from: Date()), validFrom: isoDateFormatter.string(from: Calendar.current.date(byAdding: .second, value: 1, to: Date())!), validUntil: isoDateFormatter.string(from: Calendar.current.date(byAdding: .month, value: 1, to: Date())!))
-		let mso = MobileSecurityObject(version: MobileSecurityObject.defaultVersion, digestAlgorithm: dak.rawValue, valueDigests: valueDigests, deviceKey: deviceKey, docType: document.docType, validityInfo: validityInfo)
-		return mso
-	}
+
 
 }
