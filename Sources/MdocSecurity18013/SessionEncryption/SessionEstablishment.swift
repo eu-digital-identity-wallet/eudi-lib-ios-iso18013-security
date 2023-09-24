24a0589aacb21603110a6f59004417772e448da9
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-09-24 05:49:11 +0300

    session establisment reader key optional (may be passed from reader engagement)

--- Sources/MdocSecurity18013/SessionEncryption/SessionEstablishment.swift
+++ Sources/MdocSecurity18013/SessionEncryption/SessionEstablishment.swift
@@ -8,14 +8,17 @@ import MdocDataModel18013
 /// The mdoc reader creates the session establishment message.Contains the reader key and the encrypted mdoc request.
 /// The mdoc uses the data from the session establishment message to derive the session keys and decrypt the mdoc request.
 public struct SessionEstablishment {
-	public let eReaderKeyRawData: [UInt8]
+	public var eReaderKeyRawData: [UInt8]?
 	public let data: [UInt8]
 	
 	enum CodingKeys: String, CodingKey {
 		case eReaderKey
 		case data
 	}
-	public var eReaderKey: CoseKey? { CoseKey(data: eReaderKeyRawData) }
+	public var eReaderKey: CoseKey? {
+		if let eReaderKeyRawData {
+			return CoseKey(data: eReaderKeyRawData) } else { return nil }
+	}
 }
 
 extension SessionEstablishment: CBORDecodable {
@@ -23,17 +26,19 @@ extension SessionEstablishment: CBORDecodable {
 		guard case let .map(values) = cbor else { logger.error("Session establishment data must be a map"); return nil  }
 		guard case let .byteString(bs) = values[CodingKeys.data] else { logger.error("Session establishment missing data"); return nil }
 		data = bs
-		guard case let .tagged(tag, value) = values[CodingKeys.eReaderKey] else { logger.error("Session establishment eReaderKey must be tagged"); return nil }
-		guard tag == .encodedCBORDataItem else { logger.error("Session establishment eReaderKey tag must be encodedCBOR (24)"); return nil }
-		guard case let .byteString(ebs) = value else { logger.error("eReaderKey value must be byteString"); return nil }
-		eReaderKeyRawData = ebs
+		if let eReaderKey = values[CodingKeys.eReaderKey] {
+			guard case let .tagged(tag, value) = eReaderKey else { logger.error("Session establishment eReaderKey must be tagged"); return nil }
+			guard tag == .encodedCBORDataItem else { logger.error("Session establishment eReaderKey tag must be encodedCBOR (24)"); return nil }
+			guard case let .byteString(ebs) = value else { logger.error("eReaderKey value must be byteString"); return nil }
+			eReaderKeyRawData = ebs
+		} else { eReaderKeyRawData = nil }
 	}
 }
 
 extension SessionEstablishment: CBOREncodable {
 	public func toCBOR(options: CBOROptions) -> CBOR {
 		var res = [CBOR:CBOR]()
-		res[.utf8String(CodingKeys.eReaderKey.rawValue)] = eReaderKeyRawData.taggedEncoded
+		if let eReaderKeyRawData { res[.utf8String(CodingKeys.eReaderKey.rawValue)] = eReaderKeyRawData.taggedEncoded }
 		res[.utf8String(CodingKeys.data.rawValue)] = .byteString(data)
 		return .map(res)
 	}
