813098e5cb864d276c4c21d5bbec2ee7ab0cb1fd
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-12-22 00:48:48 +0200

    Add OrderedCollections import and use OrderedDictionary in SessionData and SessionEstablishment

--- Sources/MdocSecurity18013/SessionEncryption/SessionData.swift
+++ Sources/MdocSecurity18013/SessionEncryption/SessionData.swift
@@ -18,6 +18,7 @@ import Foundation
 import MdocDataModel18013
 import SwiftCBOR
 import Logging
+import OrderedCollections
 
 /// Message data transfered between mDL and mDL reader
 public struct SessionData {
@@ -39,14 +40,14 @@ public struct SessionData {
 extension SessionData: CBORDecodable {
 	public init?(cbor: CBOR) {
 		guard case let .map(values) = cbor else { logger.error("Session data must be a map"); return nil  }
-		if case let .unsignedInt(s) = values[CodingKeys.status] { status = s } else { logger.info("SessionData: Missing status"); status = nil  }
-		if case let .byteString(bs) = values[CodingKeys.data] { data = bs } else { logger.error("SessionData: Missing data"); data = nil  }
+		if case let .unsignedInt(s) = values[.utf8String(CodingKeys.status.rawValue)] { status = s } else { logger.info("SessionData: Missing status"); status = nil  }
+		if case let .byteString(bs) = values[.utf8String(CodingKeys.data.rawValue)] { data = bs } else { logger.error("SessionData: Missing data"); data = nil  }
 	}
 }
 
 extension SessionData: CBOREncodable {
 	public func toCBOR(options: CBOROptions) -> CBOR {
-		var res = [CBOR:CBOR]()
+		var res = OrderedDictionary<CBOR, CBOR>()
 		if let st = status { res[CBOR.utf8String(CodingKeys.status.rawValue)] = CBOR.unsignedInt(st) }
 		if let d = data { res[CBOR.utf8String(CodingKeys.data.rawValue)] = CBOR.byteString(d) }
 		return .map(res)
