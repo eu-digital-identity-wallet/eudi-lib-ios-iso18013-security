54c1b4bf934a544d89b223a9ce557a411421be7e
Filippos Sakellaropoulos <phisakel@gmail.com>
2024-07-03 22:38:51 +0300

    Fix unit tests

--- Tests/MdocSecurity18013Tests/MdocSecurity18013Tests.swift
+++ Tests/MdocSecurity18013Tests/MdocSecurity18013Tests.swift
@@ -57,10 +57,10 @@ final class MdocSecurity18013Tests: XCTestCase {
         let transcript = try XCTUnwrap(SessionTranscript(cbor: st))
         let dse = try XCTUnwrap(try CBOR.decode([UInt8](Self.AnnexdTestData.d51_sessionEstablishData)))
         let se: SessionEstablishment = try XCTUnwrap(SessionEstablishment(cbor: dse))
-        var de = try XCTUnwrap(DeviceEngagement(data: transcript.devEngRawData))
+        var de = try XCTUnwrap(DeviceEngagement(data: transcript.devEngRawData!))
         de.setD(d: Self.AnnexdTestData.d51_ephDeviceKey.d)
         var sessionEncr = try XCTUnwrap(SessionEncryption(se: se, de: de, handOver: transcript.handOver))
-		sessionEncr.deviceEngagementRawData = transcript.devEngRawData // cbor encoding differs between implemenentations, for mDL with our own implementation they will be identical
+		sessionEncr.deviceEngagementRawData = try XCTUnwrap(transcript.devEngRawData) // cbor encoding differs between implemenentations, for mDL with our own implementation they will be identical
         return (se, sessionEncr)
     }
      
