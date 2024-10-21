b634d9eb19f09960ee8d343be0b54ad228125165
Filippos Sakellaropoulos <phisakel@gmail.com>
2024-10-22 02:44:42 +0300

    Refactor ECC curve switch statement for key agreement

--- Sources/MdocSecurity18013/SessionEncryption/EckaDHAgreement.swift
+++ Sources/MdocSecurity18013/SessionEncryption/EckaDHAgreement.swift
@@ -25,7 +25,7 @@ extension CoseKeyExchange {
 	public func makeEckaDHAgreement(inSecureEnclave: Bool) -> SharedSecret? {
 		var sharedSecret: SharedSecret?
 		switch publicKey.crv {
-		case .p256:
+		case .P256:
 			guard let puk256 = try? P256.KeyAgreement.PublicKey(x963Representation: publicKey.getx963Representation()) else { return nil}
 			if inSecureEnclave {
 				guard let sOID = privateKey.secureEnclaveKeyID else { logger.error("Missing Private key Secure Enclave ID"); return nil }
@@ -35,14 +35,15 @@ extension CoseKeyExchange {
 				guard let prk256 = try? P256.KeyAgreement.PrivateKey(x963Representation: privateKey.getx963Representation()) else { return nil}
 				sharedSecret = try? prk256.sharedSecretFromKeyAgreement(with: puk256)
 			}
-		case .p384:
+		case .P384:
 			guard let puk384 = try? P384.KeyAgreement.PublicKey(x963Representation: publicKey.getx963Representation()) else { return nil}
 			guard let prk384 = try? P384.KeyAgreement.PrivateKey(x963Representation: privateKey.getx963Representation()) else { return nil}
 			sharedSecret = try? prk384.sharedSecretFromKeyAgreement(with: puk384)
-		case .p521:
+		case .P521:
 			guard let puk521 = try? P521.KeyAgreement.PublicKey(x963Representation: publicKey.getx963Representation()) else { return nil}
 			guard let prk521 = try? P521.KeyAgreement.PrivateKey(x963Representation: privateKey.getx963Representation()) else { return nil}
 			sharedSecret = try? prk521.sharedSecretFromKeyAgreement(with: puk521)
+        default: fatalError("Unsuppported Curve type \(publicKey.crv)")
 		}
 		return sharedSecret
 	}	
