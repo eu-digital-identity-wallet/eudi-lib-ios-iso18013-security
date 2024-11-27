9df198100f8f82b6b72e28f7425f6118359892fc
Filippos Sakellaropoulos <phisakel@gmail.com>
2024-11-27 21:49:27 +0200

    Update Package.resolved and Package.swift to reflect new dependency revisions and locations

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
             targets: ["MdocSecurity18013"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/niscy-eudiw/eudi-lib-ios-iso18013-data-model.git", branch: "SecureArea"),
-        //.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", exact: "0.3.3"),
+        .package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", exact: "0.4.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
         .package(url: "https://github.com/apple/swift-certificates.git", .upToNextMajor(from: "1.0.0"))
     ],
